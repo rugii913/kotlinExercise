e6687f00aa528d3ba91f3ccf9123517ea1215225
rugii913 <rugii913@gmail.com>
2024-01-21 02:07:26 +0900

    종속성 추가
    현재 종속성
    - Spring Web, Spring Data JPA, QueryDSL, H2, MySQL, Swagger, starter test, Kotest, 플러그인 kapt 및 ktlint

--- project2/build.gradle.kts
+++ project2/build.gradle.kts
@@ -6,6 +6,12 @@ plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
+
+    // kapt
+    kotlin("kapt") version "1.9.22"
+
+    // ktlint
+    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
 }
 
 group = "com"
@@ -20,13 +26,34 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // spring web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // spring data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
+    // h2 db
     runtimeOnly("com.h2database:h2")
+
+    // mysql
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // openapi - swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+    // spring boot starter test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
 tasks.withType<KotlinCompile> {
